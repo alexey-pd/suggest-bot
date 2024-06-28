8d66a86a2f64dac057e1ffce1df1feab35b1d7c7
Aleksei Pudnikov <alexey.pudnikov@yandex.ru>
2024-06-28 19:32:16 +0200

    message author

--- src/bot/features/send/send.ts
+++ src/bot/features/send/send.ts
@@ -1,20 +1,21 @@
 import process from 'node:process';
+import { InlineKeyboard } from 'grammy';
 // @ts-expect-error debt
 import type { Other } from 'grammy/out/core/api';
 // @ts-expect-error debt
 import type { RawApi } from 'grammy/out/core/client';
 import type { Context } from '#root/bot/context.js';
-import { InlineKeyboard } from 'grammy';
 
 export async function sendPhoto(ctx: Context, fileId: string, other?: Other<RawApi, 'sendPhoto', 'chat_id' | 'photo'>) {
   const [adminId] = JSON.parse(`${process.env.BOT_ADMINS}`);
 
   const keyboard = new InlineKeyboard()
-    .text("Approve", 'approve');
+    .text('Approve', 'approve');
 
   const params = {
     ...other,
     reply_markup: keyboard,
+    caption: `@${ctx.from?.username || ctx.from?.first_name}`,
   };
 
   await ctx.api.sendPhoto(
